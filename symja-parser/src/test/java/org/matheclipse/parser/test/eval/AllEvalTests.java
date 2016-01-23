30a78e3fd2d69e16c51d48587b0df230556252cc
axexlck <axelclk@gmail.com>
2016-01-23 14:55:59 +0100

    Initial commit

---
+++ symja-parser/src/test/java/org/matheclipse/parser/test/eval/AllEvalTests.java
@@ -0,0 +1,23 @@
+package org.matheclipse.parser.test.eval;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllEvalTests extends TestCase {
+	public AllEvalTests(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		TestSuite s = new TestSuite();
+
+		s.addTestSuite(EvalComplexTestCase.class);
+		s.addTestSuite(EvalDoubleTestCase.class);
+		s.addTestSuite(EvalDoubleRelaxedTestCase.class);
+		s.addTestSuite(EvalComplexRelaxedTestCase.class);
+		s.addTestSuite(EvalDfpRelaxedTestCase.class);
+		return s; 
+	}
+
+}
